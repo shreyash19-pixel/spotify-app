b59d135c8e136c0277e116beb454962fc15faaa2
shreyash19-pixel <shreyashp734@gmail.com>
2023-10-14 12:10:48 +0530

    completed#1

--- src/styles/HomePage/index.jsx
+++ src/styles/HomePage/index.jsx
@@ -32,7 +32,7 @@ export const HomeWrapper = styled.div`
 
 export const HomeWrapperTablet = styled.div`
     width: 100%;
-    height :100%;
+    height :100vh;
     background: #121212;
     display: none;
     border-radius: 10px;
